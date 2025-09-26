09c92c31c7a3355e02a22d5df45f0099db21b205
Darae <darae01225@gmail.com>
2025-09-26 09:18:09 +0800

    Todo Text List component created

---
+++ .history/src/App_20250926091543.tsx
@@ -0,0 +1,7 @@
+import todoList from "./components/TodoList";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ .history/src/components/TodoList_20250926091444.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./TaskList.css";
+
+type Todo = {
+  id: string,
+  text: string,
+  status: "pending" | "completed",
+  timestamp: string,
+};
+
+export default function TaskList() {
+  const initialTodo: Todo[] = [
+    {
+      id: uuidv4(),
+      text: "grocery shopping",
+      status: "pending",
+      timestamp: new Date().toLocaleString(),
+    },
+    {
+      id: uuidv4(),
+      text: "watching movie",
+      status: "pending",
+      timestamp: new Date().toLocaleString(),
+    },
+  ];
+
+  const [todos, setTodos] = useState(initialTodo);
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id} className="todo-list">
+          <div className="todo-left">
+            <input type="checkbox" />
+            <span className="todo-text">{todo.text}</span>
+          </div>
+          <div className="todo-right">
+            <span>{todo.timestamp}</span>
+            <button>Delete</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}

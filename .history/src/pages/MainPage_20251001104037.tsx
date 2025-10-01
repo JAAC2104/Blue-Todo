3e4d2452c9326ccab011f6a450d851717195a05d
Darae <darae01225@gmail.com>
2025-10-01 10:41:31 +0800

    initial todo list removed

--- .history/src/pages/MainPage_20251001104037.tsx
+++ .history/src/pages/MainPage_20251001104037.tsx
@@ -4,6 +4,7 @@ import TaskList from "../components/TaskList";
 import Statistics from "../components/Statistics";
 import TasksHandler from "../components/TasksHandler";
 import "../styles/pages/MainPage.css";
+import { v4 as uuidv4 } from "uuid";
 import type { Todo } from "../types/Todo";
 
 export default function MainPage() {
